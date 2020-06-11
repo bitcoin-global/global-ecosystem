b24c8bf609e0a6b770c201718173e4f62c19981c
Ramiz Polić <ramiz.polic@hotmail.com>
2020-06-11 23:15:48 +0200

    src: force encryption and github oauth

--- scripts/deploy-cicd.sh
+++ scripts/deploy-cicd.sh
@@ -20,6 +20,11 @@ info "Installing dependencies..."
 sudo apt install -y xclip xsel
 
 ###############################################################################
+# ===================== REQUIREMENTS
+kill_if_empty "GITHUB_APP_ID" $GITHUB_APP_ID
+kill_if_empty "GITHUB_APP_SECRET" $GITHUB_APP_SECRET
+
+###############################################################################
 # ===================== Creating secrets
 ###############################################################################
 info "Creating secrets..."
@@ -32,13 +37,14 @@ ssh-keygen -t rsa -f worker-key  -N '' -m PEM
 ssh-keygen -t rsa -f session-signing-key  -N '' -m PEM
 rm session-signing-key.pub
 
-echo $POSTGRESQL_USER > postgresql-user
-echo $POSTGRESQL_PASS > postgresql-password
+echo "admin" > postgresql-user
+echo "$(openssl rand -base64 24)" > postgresql-password
 
-# echo $GITHUB_APP_ID > github-client-id
-# echo $GITHUB_APP_SECRET > github-client-secret
+echo $GITHUB_APP_ID > github-client-id
+echo $GITHUB_APP_SECRET > github-client-secret
 
 printf "%s" "$(openssl rand -base64 24)" > encryption-key
+printf "%s" "$(openssl rand -base64 24)" > web-encryption-key
 printf "%s:%s" "admin" "$(openssl rand -base64 24)" > local-users
 
 mkdir concourse web worker || true
@@ -52,12 +58,15 @@ mv worker-key worker/worker-key
 mv session-signing-key web/session-signing-key
 mv host-key web/host-key
 mv local-users web/local-users
+mv web-encryption-key web/encryption-key
 cp worker/worker-key-pub web/worker-key-pub
 
-# other concourse secrets
+# additional concourse secrets
 mv encryption-key concourse/encryption-key
 mv postgresql-password concourse/postgresql-password
 mv postgresql-user concourse/postgresql-user
+mv github-client-id concourse/github-client-id
+mv github-client-secret concourse/github-client-secret
 cd ..
 
 ###############################################################################
@@ -80,7 +89,7 @@ kubectl create secret generic concourse-web --from-file=$SECRET_FOLDER/web/ \
     --dry-run=client -o yaml | kubectl apply -f -
 kubectl create secret generic concourse-concourse --from-file=$SECRET_FOLDER/concourse/ \
     --dry-run=client -o yaml | kubectl apply -f -
-# rm -rf web/* worker/* concourse/*
+rm -rf $SECRET_FOLDER
 
 cat <<EOF | kubectl apply -f -
 apiVersion: networking.gke.io/v1beta2
