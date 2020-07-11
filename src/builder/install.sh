ede668074a4558d1c4643075d2fc090cf18f2a69
Ramiz Polić <ramiz.polic@hotmail.com>
2020-07-11 17:54:53 +0200

    ci: add required ruby packages

--- src/builder/install.sh
+++ src/builder/install.sh
@@ -8,7 +8,7 @@ adduser -S gkh gkh
 
 # install apk packages
 apk update
-apk --no-cache add ca-certificates gnupg mysql-client openssl ncurses jq ruby python3
+apk --no-cache add ca-certificates gnupg mysql-client openssl ncurses jq ruby python3 ruby-json
 
 # install cloud_sql_proxy & kubectl
 gcloud components install -q beta cloud_sql_proxy kubectl
