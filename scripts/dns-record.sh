85813383de0ad889705eaa31a6d108f502affd3f
Ramiz Polić <ramiz.polic@hotmail.com>
2020-07-16 17:02:44 +0200

    src: add gcp project config to dns record script

--- scripts/dns-record.sh
+++ scripts/dns-record.sh
@@ -26,6 +26,8 @@ case $i in
     shift ;;
     -t=*|--type=*)          DOMAIN_TYPE="${i#*=}"
     shift ;;
+    --project=*)            GCP_PROJECT="${i#*=}"
+    shift ;;
     --ttl=*)                TTL_VALUE="${i#*=}"
     shift ;;
     *) error "Unknown parameter passed: $i"; exit 1 ;;
@@ -34,10 +36,13 @@ done
 
 # Make sure required params are provided
 kill_if_empty "--name" $DNS_ZONE_NAME
+ADDITIONAL_CONFIG=""
+if [ ! -z "$GCP_PROJECT" ]; then
+    ADDITIONAL_CONFIG="--project $GCP_PROJECT"
+fi
 
 warn "Adding ($DOMAIN_VALUE) to DNS records ($DNS_ZONE_NAME)..."
-
-DNS_RECORDS_LIST=$(gcloud dns record-sets list -z $DNS_ZONE_NAME --format json | jq ".[] | select(.name==\"$DOMAIN_VALUE.\")")
+DNS_RECORDS_LIST=$(gcloud dns record-sets list -z $DNS_ZONE_NAME $ADDITIONAL_CONFIG --format json | jq ".[] | select(.name==\"$DOMAIN_VALUE.\")")
 PARENT_DOMAIN=$(sed 's/.*\.\(.*\..*\)/\1/' <<< $DOMAIN_VALUE)
 PARENT_VALUE=${PARENT_VALUE:-$PARENT_DOMAIN.}
 TTL_VALUE=${TTL_VALUE:-300}
@@ -47,7 +52,7 @@ if [ ! -z "$DNS_RECORDS_LIST" ]; then
     warn "Data ($DOMAIN_VALUE) already in DNS records, skipping..."
 else
     warn "Data ($DOMAIN_VALUE) not in DNS records, adding..."
-    gcloud dns record-sets transaction start --zone=$DNS_ZONE_NAME
-    gcloud dns record-sets transaction add "$PARENT_VALUE" --name="$DOMAIN_VALUE." --ttl=$TTL_VALUE --type=$DOMAIN_TYPE --zone=$DNS_ZONE_NAME
-    gcloud dns record-sets transaction execute --zone=$DNS_ZONE_NAME
+    gcloud dns record-sets transaction start $ADDITIONAL_CONFIG --zone=$DNS_ZONE_NAME
+    gcloud dns record-sets transaction add "$PARENT_VALUE" --name="$DOMAIN_VALUE." --ttl=$TTL_VALUE --type=$DOMAIN_TYPE $ADDITIONAL_CONFIG --zone=$DNS_ZONE_NAME
+    gcloud dns record-sets transaction execute $ADDITIONAL_CONFIG --zone=$DNS_ZONE_NAME
 fi
